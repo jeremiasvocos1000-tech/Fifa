6cfd14bea3a8b08c306566adbeffa5ee5a829e24
tomas arias <ma249545@gmail.com>
2026-07-29 11:06:12 -0300

    benja

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Hola putita </h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
@@ -37,8 +37,8 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <h2>Messi goat prime tung tung sahur </h2>
+          <p>Your messi, answered</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
