703075d6385a31e52bf3c71f74eb0118eac2616f
MaxCodeus <imaginvor1@gmail.com>
2026-07-30 08:40:21 -0300

    resuelvo conflicto usando version remota

--- frontend/src/paginas/login.jsx
+++ frontend/src/paginas/login.jsx
@@ -1,79 +1,43 @@
 import { useState } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-
-
-import App from '/paginas/App.jsx'
-
+import "./CSS/login.css";
 
 function Login() {
+
+  const navigate = useNavigate();
+
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
-  const [logResult, setLogResult] = useState(null);
-
-
-  const trueUser = "Admin";
-  const truePassword = "1234";
-
 
   const enviar = (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-
-  if (User === TrueUser && Password === TruePassword) {
-
-
-    setLogResult(true);
-
-
-    setTimeout(() => {
-      navigate("/inicio");
-    }, 2000);
-
-
-  } else {
-
-
-    setLogResult(false);
-
-
-  }
-};
-
+    if (user === "Admin" && password === "1234") {
+      navigate("/");
+    }
+  };
 
   return (
- 
-    <>
-      <form onSubmit={enviar}>
-        <input
-          type="text"
-          placeholder="Usuario"
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
-        />
+    <form onSubmit={enviar}>
+      <input
+        placeholder="Usuario"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
 
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-
-        <button type="submit">Enviar</button>
-      </form>
-
-
-    {logResult === true && (<p>Inicio de sesión correcto</p>)}
-
-
-{logResult === false && (<p>Usuario o contraseña incorrectos</p>)}
-    </>
+      <button>
+        Entrar
+      </button>
+    </form>
   );
 }
-
 
 export default Login;
